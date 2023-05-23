d804834b7565186ee160e733203250b815cdbf1f
Santiago-Poggio <129192655+Santiago-Poggio@users.noreply.github.com>
2023-05-23 12:51:16 -0300

    Update jupyter-streamlit.py

--- jupyter-streamlit.py
+++ jupyter-streamlit.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+
 conexiones = ""
 conexiones = pd.read_csv("./ciencia_de_datos_en_escuelas/conectividad_argentina/Conectividad_Internet.csv")
 conexiones.head(20)
 st.write(conexiones.head(20))
-SATELITAL=conexiones['SATELITAL'].value_counts().rename_axis('SATELITAL').reset_index(name='Localidades')
-SATELITAL.style.hide(axis='index') 
-st.write(SATELITAL)
