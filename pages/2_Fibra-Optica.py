a9580ae855049cb86ece8d4096d2d7a520ef689a
Santiago-Poggio <129192655+Santiago-Poggio@users.noreply.github.com>
2023-05-30 12:52:13 -0300

    Update 2_Fibra-Optica.py

--- pages/2_Fibra-Optica.py
+++ pages/2_Fibra-Optica.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
 conexiones = ""
 conexiones = pd.read_csv("./ciencia_de_datos_en_escuelas/conectividad_argentina/Conectividad_Internet.csv")
@@ -10,3 +11,21 @@ st.write (FIBRAOPTICA)
 
 torta = px.pie(FIBRAOPTICA, values='Localidades', names='Localidades', title='Cantidad de localidades con Fibra Optica', width=800, height=800)
 st.write(torta)
+
+mapa_Fibra = conexiones[["FIBRAOPTICA","Latitud","Longitud"]]
+si = ['SI']
+mask1 = mapa_Fibra[mapa_Fibra['FIBRAOPTICA'].isin(si)]
+
+fig = go.Figure(data=go.Scattergeo(
+        lon = mask1['Longitud'],
+        lat = mask1['Latitud'],
+        text = mask1,
+        mode = 'markers',
+        ))
+
+fig.update_layout(
+        title = 'Fibraoptica locations',
+        geo_scope='south america',
+    )
+fig.update_geos(fitbounds="locations") 
+st.write(fig)
