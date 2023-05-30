cc6fcbea6db190cb728b2404f16bff96b62ed41b
Santiago-Poggio <129192655+Santiago-Poggio@users.noreply.github.com>
2023-05-30 12:52:57 -0300

    Update 3_4G.py

--- pages/3_4G.py
+++ pages/3_4G.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
 conexiones = ""
 conexiones = pd.read_csv("./ciencia_de_datos_en_escuelas/conectividad_argentina/Conectividad_Internet.csv")
@@ -11,3 +12,22 @@ st.write (C4G)
 
 torta = px.pie(C4G, values='Localidades', names='Localidades', title='Cantidad de localidades con 4G', width=800, height=800)
 st.write (torta)
+
+mapa_4G = conexiones[["4G","Latitud","Longitud"]]
+si = ['SI']
+mask = mapa_4G[mapa_4G['4G'].isin(si)]
+
+
+fig = go.Figure(data=go.Scattergeo(
+    lon = mask['Longitud'],
+    lat = mask['Latitud'],
+    text = mask,
+    mode = 'markers',
+    ))
+
+fig.update_layout(
+        title = '4G locations',
+        geo_scope='south america',
+    )
+fig.update_geos(fitbounds="locations") 
+st.write(fig)
